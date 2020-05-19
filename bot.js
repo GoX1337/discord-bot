02297c8e855e99cbbabf130417617f7eac3ce15a
gox <gox.roche@gmail.com>
2020-05-19 18:12:16 +0200

    Play mp3 on msg event

--- bot.js
+++ bot.js
@@ -1,18 +1,34 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const fs = require('fs');
 const client = new Discord.Client();
 
 client.login(process.env.DISCORD_TOKEN);
 
 client.once('ready', () => {
-	console.log('Gox discord bot ready.');
+	console.log(`${client.user.tag} discord bot is ready.`);
 });
 
-client.on('message', async message => {
-    console.log(`Message from ${message.author.username}: ${message.content}`);
+client.on("disconnected", () => {
+	console.log("Disconnected!");
+	process.exit(1);
+});
 
-    if (message.member && message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-        console.log(`Join voice channel "${message.member.voice.channel.name}"`);
+client.on('error', error => {
+	console.error(error);
+});
+
+let connection = null;
+
+client.on('message', async message => {
+	console.log(`Message from ${message.author.username}: ${message.content}`);
+
+	if (message.member && message.member.voice.channel && message.author.username === "GoX") {
+		console.log(`Join voice channel "${message.member.voice.channel.name}"`);
+		connection = await message.member.voice.channel.join();
+		connection.play("roblox-death.mp3");
+		connection.on('speaking', (user, speaking) => {
+			console.log(user.username, speaking);
+		});
 	}
 });
